422d8c3573aaf6978f099bf0522001904b0f52a7
S. Suzuki <terrierscript@gmail.com>
2022-05-21 01:59:24 +0900

    why safari people

--- src/components/page/IssueChatPage.tsx
+++ src/components/page/IssueChatPage.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Box, Center, Divider, Flex, Grid, HStack, Link, Spacer, Spinner, Stack, Textarea } from "@chakra-ui/react"
-import { FC, Suspense } from "react"
+import { FC, Suspense, useMemo } from "react"
 import { IssueResponse } from "../../services/github/client"
 import { ChatInputArea } from "./ChatInput"
 import { IssuePageProps } from "./Props"
@@ -38,11 +38,14 @@ const Issue: FC<{ issue: IssueResponse }> = ({ issue }) => {
 
 
 const IssueStream: FC<{ issues: IssueResponse[] }> = ({ issues }) => {
-  return <Stack spacing={4}>
+  const stream = useMemo(() => {
+    return issues?.concat().reverse()
+  }, [])
+  return <Stack spacing={4} >
     <Spacer
     // minH="100vh"
     />
-    {issues?.concat().reverse().map((issue, key) => {
+    {stream.map((issue, key) => {
       return <Issue issue={issue} key={key} />
     })}
   </Stack>
@@ -66,7 +69,16 @@ const IssueStreamWrap: FC<IssuePageProps> = ({ owner, repo, filter }) => {
       <Spinner />
     </Center>
   }
-  return <IssueStream issues={data.issues} />
+  return <Flex
+    overflow="scroll"
+    w="100%"
+    h="100%"
+    p={4}
+    flexDirection="column-reverse"
+  >
+    <IssueStream issues={data.issues} />
+  </Flex >
+
 }
 export const IssueChatPage: FC<IssuePageProps> = ({ owner, repo, filter }) => {
   return <Box
@@ -80,15 +92,7 @@ export const IssueChatPage: FC<IssuePageProps> = ({ owner, repo, filter }) => {
     // h={height ?? "100%"}
     >
       <ChatHeader {...{ owner, repo, filter }} />
-      <Flex
-        overflow="scroll"
-        w="100%"
-        h="100%"
-        p={4}
-        flexDirection="column-reverse"
-      >
-        <IssueStreamWrap  {...{ owner, repo, filter }} />
-      </Flex >
+      <IssueStreamWrap  {...{ owner, repo, filter }} />
       <Box bg="gray.200" p={2}>
         <ChatInputArea {...{ owner, repo }} />
       </Box>
