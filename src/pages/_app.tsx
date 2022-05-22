e99ddbd91be8d1317d03d1ef23f299150919bebd
S. Suzuki <terrierscript@gmail.com>
2022-05-22 17:53:51 +0900

    append performance checker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import { SessionProvider } from "next-auth/react"
-import { ChakraProvider } from "@chakra-ui/react"
+import { Box, ChakraProvider } from "@chakra-ui/react"
 import React, { } from "react"
 import { appTheme } from "../services/chakra/theme"
 import { AppProps } from "next/app"
 import { NeedLayout } from "../components/layout/NeedLayout"
 import Head from "next/head"
 import { AppSWRPRovider } from "../services/swr/AppSWRProvider"
-import { IconContext } from "react-icons"
 
 function MyApp({ Component, pageProps: { session, ...pageProps },
 }: AppProps) {
@@ -14,23 +13,25 @@ function MyApp({ Component, pageProps: { session, ...pageProps },
     <>
       <Head>
         <title>chat issue</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
       <ChakraProvider theme={appTheme}>
-        <AppSWRPRovider>
+        <Box bg="red.100">
 
-          {/* <IconContext.Provider value={{ color: "white" }}> */}
-          {/* <Suspense fallback={<Box>...</Box>}> */}
-          <SessionProvider session={session}>
-            <NeedLayout>
-
-              <Component {...pageProps} />
-            </NeedLayout>
-          </SessionProvider>
-          {/* </Suspense> */}
-          {/* </IconContext.Provider> */}
-        </AppSWRPRovider>
+          <AppSWRPRovider>
+            <Box bg="blue.100">
+              <SessionProvider session={session}>
+                <Box bg="green.100">
+                  <NeedLayout>
+                    <Box bg="yellow.100">
+                      <Component {...pageProps} />
+                    </Box>
+                  </NeedLayout>
+                </Box>
+              </SessionProvider>
+            </Box>
+          </AppSWRPRovider>
+        </Box>
       </ChakraProvider >
     </>
   )
