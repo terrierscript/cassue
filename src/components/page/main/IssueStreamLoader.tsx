50f24a3356dfd3d56b11abe95f09ff8dc86197cb
S. Suzuki <terrierscript@gmail.com>
2022-05-25 21:21:29 +0900

    split file

--- src/components/page/main/IssueStreamLoader.tsx
+++ src/components/page/main/IssueStreamLoader.tsx
@@ -2,14 +2,15 @@ import { FC } from "react"
 import { useIssues } from "../apiHooks"
 import { IssueStream } from "./Stream"
 import { useChatRouteParam } from "../useChatRouteParam"
-import { Loading, StreamContainer } from "./ChatStream"
+import { StreamContainer } from "./StreamContainer"
+import { StreamLoading } from "./StreamLoading"
 
 
 export const IssueStreamLoader: FC<{}> = ({ }) => {
   const { owner, repo, filter } = useChatRouteParam()
   const { data } = useIssues({ owner, repo, filter })
   if (!data) {
-    return <Loading />
+    return <StreamLoading />
   }
   return <StreamContainer>
     <IssueStream issues={data.issues} />
