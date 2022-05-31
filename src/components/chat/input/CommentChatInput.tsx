027c95446e8dadf32f130316c95cdf4407728f14
S. Suzuki <terrierscript@gmail.com>
2022-05-31 22:43:07 +0900

    add

--- src/components/chat/input/CommentChatInput.tsx
+++ src/components/chat/input/CommentChatInput.tsx
@@ -1,63 +1,35 @@
-import { Box, HStack, IconButton, Input } from "@chakra-ui/react"
-import { FC, useState } from "react"
-import { CommentPost, IssuePost } from "../../../services/github/Schema"
+import { Box } from "@chakra-ui/react"
+import { FC } from "react"
+import { CommentPost } from "../../../services/github/Schema"
 import { useIssueComments, useIssues } from "../../page/apiHooks"
-import { resolveFilterToPost } from "../../../services/github/resolveFilter"
 import { useChatRouteParam, useCommentNumber, useFilterValue } from "../../page/useChatRouteParam"
 import { alphaBgStyle } from "../../atomic/styleUtils"
 import { CommentDiscussionIcon } from '@primer/octicons-react'
 import { ReadOnlyGuard } from "./ReadOnly"
-import { useForm } from "react-hook-form"
+import { ChatInput } from "./ChatInput"
 
-const ChatInput: FC<{ onSubmit: (value: string) => void }> = ({ onSubmit }) => {
-  // const [value, setValue] = useState("")
-  const { register, handleSubmit, formState, reset } = useForm()
-
-  return <form onSubmit={handleSubmit(async (data) => {
-    await onSubmit(data.value)
-    reset()
-  })}>
-    <HStack>
-      <Input
-        disabled={formState.isSubmitting}
-        _light={{
-          bg: "whiteAlpha.800"
-        }}
-        _dark={{
-          bg: "blackAlpha.800"
-        }}
-        {...register("value")}
-      />
-      <IconButton
-        disabled={formState.isSubmitting}
-        type="submit"
-        icon={<CommentDiscussionIcon />}
-        aria-label={"Post"}
-      />
-    </HStack>
-  </form>
-}
-
-
+// const ChatInput: FC<{ o
 const InputSending: FC<{ number: number }> = ({ number }) => {
   const { owner, repo, filter } = useChatRouteParam()
   const { target, value } = useFilterValue()
   const { mutate } = useIssues({ owner, repo, target, value })
   const { mutate: mutateComment } = useIssueComments({ owner, repo, number })
-  return <ChatInput onSubmit={async (v) => {
-    // const resolvedParams = resolveFilterToPost(filter)
-    const issue: CommentPost = { body: v }
-    const result = await fetch(`/api/comments/${owner}/${repo}/${number}`, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(issue)
-    })
-    mutate()
-    mutateComment()
-    return result
-  }} />
+  return <ChatInput
+    icon={<CommentDiscussionIcon />}
+    onSubmit={async (v) => {
+      // const resolvedParams = resolveFilterToPost(filter)
+      const issue: CommentPost = { body: v }
+      const result = await fetch(`/api/comments/${owner}/${repo}/${number}`, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(issue)
+      })
+      mutate()
+      mutateComment()
+      return result
+    }} />
 }
 
 
