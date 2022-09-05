43d7eb65dea8e4c7d5a10da19f441c2a85a41392
S. Suzuki <terrierscript@gmail.com>
2022-09-05 23:52:32 +0900

    add

--- src/components/page/right/LabelSelector.tsx
+++ src/components/page/right/LabelSelector.tsx
@@ -1,12 +1,12 @@
-import { Box, Link, Wrap } from "@chakra-ui/react"
+import { Box, HStack, IconButton, Link, Wrap } from "@chakra-ui/react"
 import { FC } from "react"
 import { IssueResponse } from "../../../services/github/GithubClient"
-import { NextLink } from "../../../services/next/components"
 
+import { DuplicateIcon } from '@primer/octicons-react'
 
 
 export const LabelSelector: FC<{ issue: IssueResponse }> = ({ issue }) => {
-  return <Box>
+  return <HStack>
     <Wrap>
       {(issue.labels).flat(1).map(label => {
         const labelName = typeof label === "string" ? label : label.name
@@ -15,6 +15,13 @@ export const LabelSelector: FC<{ issue: IssueResponse }> = ({ issue }) => {
         </Box>
       })}
     </Wrap>
+    <IconButton
+      colorScheme={"gray"}
+      icon={<DuplicateIcon />}
+      aria-label={"Edit label"}
+      size="xs"
+      variant={"link"}
+    />
 
-  </Box>
+  </HStack>
 }
