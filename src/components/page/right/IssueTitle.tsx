3fcb15c80695cabd0d5997426fdb04fb192608d1
S. Suzuki <terrierscript@gmail.com>
2022-10-25 23:02:11 +0900

    add target blank

--- src/components/page/right/IssueTitle.tsx
+++ src/components/page/right/IssueTitle.tsx
@@ -32,7 +32,7 @@ export const IssueTitle: FC<{ issue: IssueNumberResponse }> = ({ issue }) => {
   }
 
   return <HStack>
-    <Link href={issue?.html_url}>
+    <Link href={issue?.html_url} target="_blank">
       <Heading size="sm">
         {issue?.title}
       </Heading>
