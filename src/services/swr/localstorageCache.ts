f20ac52d7e2d95d3b952fe9617e5272fd80f9efd
S. Suzuki <terrierscript@gmail.com>
2023-04-22 00:44:22 +0900

    add

--- src/services/swr/localstorageCache.ts
+++ src/services/swr/localstorageCache.ts
@@ -1,18 +1,18 @@
 import { Cache } from "swr"
 const KEY_NAME = "app-cache-v2"
 
-const save = (map: Map<unknown, unknown>) => {
+const save = (map: Map<string, any>) => {
   console.log("save")
   const appCache = JSON.stringify(Array.from(map.entries()))
   localStorage.setItem(KEY_NAME, appCache)
 }
 
-export function localStorageProvider() {
+export function localStorageProvider(cache: Readonly<Cache>): Cache {
   // for SSR
   if (typeof window === "undefined") {
     return new Map()
   }
-  const map = new Map(JSON.parse(localStorage.getItem(KEY_NAME) || '[]'))
+  const map: Map<string, any> = new Map(JSON.parse(localStorage.getItem(KEY_NAME) || '[]'))
   window.addEventListener('beforeunload', () => {
     save(map)
   })
